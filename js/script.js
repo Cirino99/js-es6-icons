e93bde77edcd9c80503ecc2ddcf13c176a0d3373
Simone <cirino.simone99@gmail.com>
2022-05-19 18:01:53 +0200

    prima versione

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
 // costanti dom
 const myGrid = document.getElementById('grid');
-myArr.forEach(element => {
-    const card = document.createElement('div');
-    const icon = document.createElement('i');
-    const nome = document.createElement('p');
-    icon.classList.add(element.family);
-    icon.classList.add(element.prefix + element.name);
-    console.log(icon);
-    nome.innerText = element.name;
-    card.append(icon);
-    card.append(nome);
-    myGrid.append(card);
-});
+const select = document.getElementById('filtro-tipo');
+
+select.addEventListener('change',selectArray);
+// manipolazion e del dom
+stampaElementiArr(myArr);
+function selectArray(){
+    console.log(select.value);
+    switch(select.value){
+        case 'animal':
+            const arrAnimal = myArr.filter((element)=>element.type === 'animal');
+            stampaElementiArr(arrAnimal);
+            break;
+        case 'vegetable':
+            const arrVegetable = myArr.filter((element)=>element.type === 'vegetable');
+            stampaElementiArr(arrVegetable);
+            break;
+        case 'user':
+            const arrUser = myArr.filter((element)=>element.type === 'user');
+            stampaElementiArr(arrUser);
+            break
+        default:
+            stampaElementiArr(myArr);
+    }
+}
+
+
+function stampaElementiArr(arr){
+    myGrid.innerHTML = '';
+    arr.forEach(element => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const icon = document.createElement('i');
+        const nome = document.createElement('p');
+        icon.classList.add(element.family);
+        icon.classList.add(element.prefix + element.name);
+        icon.classList.add('card-icon');
+        console.log(icon);
+        nome.innerText = element.name.toUpperCase();
+        card.append(icon);
+        card.append(nome);
+        myGrid.append(card);
+    });
+}
